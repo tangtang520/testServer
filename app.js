7651a518f536f665c33db72d43975869f928095b
creater <1315803594@qq.com>
2015-07-07 17:36:27 +0800

    change01

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var wechat = require('wechat');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -8,6 +9,12 @@ var bodyParser = require('body-parser');
 var adminRoutes = require('./routes/adminRoutes');
 var interface = require('./routes/interface');
 var app = express();
+//设置配置信息
+var config = {
+  token: 'tangtang',
+  appid: 'wx78a6a6ceff29e2e3',
+  encodingAESKey: 'efae563442df1d7504efd14827b510b6'
+};
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 12333);
@@ -21,6 +28,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 //这句话必须加
 app.use("/public", express.static(__dirname + '/public'));
+//微信内容
+app.use(express.query());
+app.use('/wechat',wechat(config,function(req,res,next){
+  var message = req.weixin
+  console.log("message--->>>",message)
+}))
 //app.get('/', routes.index);
 //app.get('/users', users.list);
 app.all('/', adminRoutes.index);
